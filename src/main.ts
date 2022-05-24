c779c92356a63aa8ca3c5c1d0359761242b9a492
lanchao <cuilanchao@sina.com>
2022-05-24 11:15:00 +0800

    main

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: lanchao
  * @Date: 2022-04-14 11:21:23
- * @LastEditTime: 2022-05-22 12:42:19
+ * @LastEditTime: 2022-05-24 11:04:43
  * @LastEditors: lanchao
  * @Reference:
  */
@@ -11,11 +11,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
 const app = createApp(App)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
 installElementPlus(app)
 app.use(store).use(router).mount('#app')
